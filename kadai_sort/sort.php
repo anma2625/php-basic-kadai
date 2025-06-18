5fcdd73e9cbbe57bd2e54cbc756982068a9b2d43
anma2625 <harumun0720@yahoo.co.jp>
2025-06-18 23:46:10 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_sort</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    function sort_2way($array,$order){
+      if($order){
+        echo"昇順にソートします。<br>";
+        sort($array);
+      } else{
+        echo"昇順にソートします。<br>";
+        rsort($array);
+      }
+
+      foreach ($array as $num){
+        echo $num . '<br>';
+      }
+    }
+
+    $nums = [15,4,8,23,10];
+
+    sort_2way($nums, true);
+    echo "<hr>";
+    sort_2way($nums,false);
+    ?>
+    </p>
+
+  
+</body>
+</html>
