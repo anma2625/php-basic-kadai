4061cf66562375c1f67d8573709db121330247e5
anma2625 <harumun0720@yahoo.co.jp>
2025-06-23 23:25:34 +0900

    Add files via upload

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -16,50 +16,46 @@ class Food {
   private $price;
 
   //コンストラクタ定義
-  public function __construct(string $name,int $price) {
+  public function __construct(string $name, int $price) {
     $this->name = $name;
     $this->price = $price;
   }
 
-  //メソッドを追加
+
+//メソッドを定義する
   public function show_price() {
-    echo "The price is:" .$this->price;
+    echo $this->price . '<br>';
   }
 }
-  
+
+//クラスの定義２
+class Animal {
+//プロパティを定義する
+private $name;
+private $height;
+private $weight;
+
+
+//コンストラクタを定義する
+public function __construct(string $name, int $height, int $weight) {
+  $this->name = $name;
+  $this->height = $height;
+  $this->weight = $weight;
+}
+
+//メソッドを定義する
+public function show_height(){
+  echo $this->height . '<br>';
+}
+}
+
+
 //インスタンス化
 $food = new Food('potato', 250);
-
 print_r($food);
 echo '<br>';
 
-//メソッドを呼ぶ
-$food->show_price();
-
-?>
-<br>
-<?php
-
-class Animal {
-  //プロパティ
-  private $name;
-  private $height;
-  private $weight;
-
-  //コンストラクタ定義
-  public function __construct(string $name, int $height,int $weight){
-    $this->name = $name;
-    $this->height = $height;
-    $this->weight = $weight;
-  }
-}
-
-//インスタンス化
-$Food = new Food('potato', 250);
-print_r($Food);
-echo '<br>';
-
-$Animal = new Animal('dog',60, 5000);
+$Animal =new Animal('dog', 60 ,5000);
 print_r($Animal);
 echo '<br>';
 
@@ -67,6 +63,9 @@ $Food->show_price();
 echo '<br>';
 $Animal->show_height();
 
+
 ?>
+</p>
 </body>
+
 </html>
